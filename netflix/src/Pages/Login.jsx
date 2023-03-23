9de7c45d7d59ec2c43c545677b5187039b4d69ed
Ankit Nayan <ankitnayan83@gmail.com>
2023-03-23 20:23:56 +0530

    commit

---
+++ netflix/src/Pages/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { login } from "../AuthContext/apiCalls";
+import { AuthContext } from "../AuthContext/Authcontext";
+import "./Login.scss";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { dispatch } = useContext(AuthContext);
+
+  const handelLogin = (e) => {
+    e.preventDefault();
+    login({ email, password }, dispatch);
+  };
+
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+            className="logo"
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form action="">
+          <h1>Sign In</h1>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handelLogin}>Sign in</button>
+          <span>
+            New to netflix
+            <Link className="link" to="/register">
+              <b>Sign up now</b>
+            </Link>
+          </span>
+          <small>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </small>
+        </form>
+      </div>
+    </div>
+  );
+};
